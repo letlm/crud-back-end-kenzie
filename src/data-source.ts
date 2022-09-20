e23ceb58930e0b724e67daf978cf47ef73b0f268
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-09-20 09:56:23 -0300

    feature: configuração heroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,30 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-export const AppDataSource =
+export const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
-    ? new DataSource({
+    ? {
         type: "sqlite",
         database: ":memory:",
-        entities: ["src/entities/*.ts"],
         synchronize: true,
-      })
-    : new DataSource({
-        type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
-        synchronize: false,
         entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });
+      }
+    : {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: false,
+        logging: true,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/migrations/*.js"]
+            : ["src/migrations/*.ts"],
+      }
+);
